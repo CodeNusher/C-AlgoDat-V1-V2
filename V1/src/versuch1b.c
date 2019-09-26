5a670969c4637b3b3e4c1e64edb9cbbe5072c4ce
Thomas Pongratz <pliss@gmx.de>
2019-09-27 00:58:53 +0200

    V1b hinzugefügt

--- V1/src/versuch1b.c
+++ V1/src/versuch1b.c
@@ -3,24 +3,34 @@
  *
  *  Created on: 27.09.2019
  *      Author: pliss
+ *
+ *      ASCII-Zeichen 127(dezimaler Wert) entspricht DEL
+ *
+ *      0 bis 32 und DEL sind keine darstellbaren ASCII-Zeichen
  */
 
 
 #include <stdio.h>
 
 int main(void){
-	int i;
+	int i = 0;
 	char ch;
-	for(i = 0; i <= 3; i++){
-		int j;
-		for(j = 0; j <= 7; j++)
+	while(i < 4){
+		int j = 0;
+		while(j < 8)
 		{
 			ch = 65 + i * 8 + j;
-			putchar(ch);
-			putchar(ch + 0x20);
-			putchar(' ');
+			if(ch >= 65 && ch <= 90){
+				putchar(ch);
+				putchar(ch + 0x20);
+				putchar(' ');
+			}
+			else
+				putchar(' ');
+			j++;
 		}
 		putchar('\n');
+		i++;
 	}
 
 	return 0;
