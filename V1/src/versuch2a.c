2e1d80cb3a0b6931075044d55102dbb98c30f817
Thomas Pongratz <pliss@gmx.de>
2019-09-27 08:29:43 +0200

    versuch2.c hinzugefügt

--- V1/src/versuch2a.c
+++ V1/src/versuch2a.c
@@ -6,3 +6,28 @@
  */
 
 
+#include <stdio.h>
+
+int main(void){
+
+	int ch;
+	for(ch = 65; ch <= 96; ch++){
+		if((ch-65) % 8 == 0){
+			putchar('\n');
+		}
+		else{
+			if(ch < 65 || ch > 90){
+				putchar(ch);
+				putchar(' ');
+				putchar(' ');
+			}
+			else{
+				putchar(ch);
+				putchar(ch + 0x20);
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return 0;
+}
